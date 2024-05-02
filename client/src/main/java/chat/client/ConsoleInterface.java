8860c9e64c57396e49070c55df7e1f6bab569cc8
Yoav <yoavklein25@gmail.com>
2024-05-02 16:18:11 +0300

    Local class

--- client/src/main/java/chat/client/ConsoleInterface.java
+++ client/src/main/java/chat/client/ConsoleInterface.java
@@ -18,40 +18,39 @@ public class ConsoleInterface implements UserInterface {
     IDGenerator idGenerator;
     ConsoleMenu consoleMenu = new ConsoleMenu("Main Menu");
     
-    class GeneralCommand implements Command {
-        Option option;
-        RequestManager.RequestContainer currentRequest;
-
-        public GeneralCommand(Option option, RequestManager.RequestContainer currentRequest) {
-            this.option = option;
-            this.currentRequest = currentRequest;
-        }
     
-        public void execute() {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            Map<Integer, Parameter> parameters = option.getParameters();
-            Map<Integer, Object> values = new HashMap<>();
-            
-            for(Integer id : parameters.keySet()) {
-                Parameter parameter = parameters.get(id);
-                System.out.println(parameter.getDescription());
-                try {
-                    String userInput = reader.readLine();
-                    Object value = parameter.getType().getConstructor(String.class).newInstance(userInput);
-                    values.put(id, value);
-                } catch(IOException e) {
-                    e.printStackTrace();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            Request request = option.createRequest(values);
-            currentRequest.currRequest = request;
-        }
-    }
-
-
     ConsoleInterface (RequestManager reqManager, IDGenerator idGenerator) {
+        class GeneralCommand implements Command {
+            Option option;
+            RequestManager.RequestContainer currentRequest;
+    
+            public GeneralCommand(Option option, RequestManager.RequestContainer currentRequest) {
+                this.option = option;
+                this.currentRequest = currentRequest;
+            }
+        
+            public void execute() {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                Map<Integer, Parameter> parameters = option.getParameters();
+                Map<Integer, Object> values = new HashMap<>();
+                
+                for(Integer id : parameters.keySet()) {
+                    Parameter parameter = parameters.get(id);
+                    System.out.println(parameter.getDescription());
+                    try {
+                        String userInput = reader.readLine();
+                        Object value = parameter.getType().getConstructor(String.class).newInstance(userInput);
+                        values.put(id, value);
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                Request request = option.createRequest(values);
+                currentRequest.currRequest = request;
+            }
+        }
         this.idGenerator = idGenerator;
         this.reqManager = reqManager;
 
